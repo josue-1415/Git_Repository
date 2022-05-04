287f6b55707b4217a69d2a036241e5d686563666
Jose Isaula <isaulajosue504@gmail.com>
2022-05-04 08:49:38 -0600

    Modificando el archivo ciclomientras.cpp

---
+++ ciclomientras.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,i;
+
+    
+    while(i<=10){
+        cout<<"Ingrese el primer numero: "<<endl; 
+        cin>>a;
+        cout<<"Ingrese el segundo numero: "<<endl;
+        cin>>b;
+        i++;
+    }
+}
